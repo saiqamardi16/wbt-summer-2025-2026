237b80bf8248d5eb398ef23c0b4c789c8d8f4471
saiqamardi16 <saiqamardia@gmail.com>
2026-07-16 20:11:46 +0600

    25-62993-2

---
+++ JS/console.js
@@ -0,0 +1,23 @@
+var name = "AIUB";
+var name = "BUET";
+console.log("University: " + name);
+
+var x;
+console.log(typeof x);
+x = 42;
+console.log(typeof x);
+x = "Hello";
+console.log(typeof x);
+
+var a = 10;
+var b = 5;
+console.log(a + b * 2);
+console.log((a + b) * 2);
+
+var a = 10;
+var b = "10";
+console.log(a == b);
+console.log(a === b);
+
+console.log(0.2 + 0.1);
+console.log((0.2 * 10 + 0.1 * 10) / 10);
